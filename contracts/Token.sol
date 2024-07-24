e43bd771b4d35dee867e4d3aa418a05148bfaf40
Seputar Testnet <99035315+seputartestnet@users.noreply.github.com>
2024-07-24 11:33:14 +0700

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("IzzyToken","IZZY"){} 
+    constructor()ERC20("Percobaan","PRC"){} 
 
-    function mint1000tokens() public {
-        _mint(msg.sender,1000*10**18);
+    function mint1000000000tokens() public {
+        _mint(msg.sender,1000000000*10**18);
     }
 
-    function burn1000tokens() public{
+    function burn1000000000tokens() public{
         _burn(msg.sender,1000*10**18);
     }
     
