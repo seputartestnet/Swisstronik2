bd0a86e95eef3e9b8041bd09c07f308e4437a5a9
Seputar Testnet <99035315+seputartestnet@users.noreply.github.com>
2024-07-25 22:20:09 +0700

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,11 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract TestToken is ERC20 {
     constructor()ERC20("Percobaan","PRC"){} 
 
-    function mint1000000000tokens() public {
-        _mint(msg.sender,1000000000*10**18);
+    function mint1000tokens() public {
+        _mint(msg.sender,1000*10**18);
     }
 
-    function burn1000000000tokens() public{
+    function burn1000tokens() public{
         _burn(msg.sender,1000*10**18);
     }
     
